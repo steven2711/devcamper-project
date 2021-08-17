80f11e8d6cebd5277f18c55e873485564ef536fd
Steven Watson <swatson.emu@gmail.com>
2021-08-17 16:09:59 -0500

    Clean up async CRUD methods.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const dotenv = require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
 const app = express();
+const colors = require("colors");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+// Body parser //
+app.use(express.json());
 
 // Dev logging middleware //
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-// Connect to database
+// Connect to database //
 connectDB();
 
 // Route files //
@@ -18,18 +23,22 @@ const bootcamps = require("./routes/bootcamps");
 // Mount routers //
 app.use("/api/v1/bootcamps", bootcamps);
 
+// Error handling
+app.use(errorHandler);
+
 // Server //
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
 
   // Close server and exit process.
   server.close(() => process.exit(1));
